5552fba7d3edc09dd3ed3393323d930dd54d323b
Jack Flowers <jakaf1@gmail.com>
2024-10-26 10:19:42 -0700

    clean up deploy vm path

--- deployment/deploy_vm.sh
+++ deployment/deploy_vm.sh
@@ -5,8 +5,7 @@ mix build && \
 tar --no-xattrs -czvf /tmp/sour_shark.tar.gz -C output . && \
 scp /tmp/sour_shark.tar.gz root@digital-ocean:/tmp && \
 ssh digital-ocean << EOF
-rm -rf ~/sites/sour_shark/
-mkdir -p ~/sites/sour_shark/
+rm -rf /var/lib/caddy/sour_shark/
 mkdir -p /var/lib/caddy/sour_shark/
 tar -xzvf /tmp/sour_shark.tar.gz -C /var/lib/caddy/sour_shark/ && \
 rm /tmp/sour_shark.tar.gz
